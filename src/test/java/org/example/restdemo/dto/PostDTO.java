edd4277aff5762e7211d2d1e332b4d74676dd425
Joe <jgliniecki@gmail.com>
2019-12-12 09:53:34 -0600

    Add more examples

---
+++ src/test/java/org/example/restdemo/dto/PostDTO.java
@@ -0,0 +1,44 @@
+package org.example.restdemo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PostDTO {
+
+    private int id;
+    private String title;
+    private String body;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
